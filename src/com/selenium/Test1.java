f4023d3c8787c5a1e2d9a3745f42fba1ff2df4ea
nagaveni <tanukunagaveni@gmail.com>
2024-04-26 15:14:39 +0530

    added string

--- src/com/selenium/Test1.java
+++ src/com/selenium/Test1.java
@@ -4,6 +4,10 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String name = "nagaveni";
+		
+		
 System.out.println("hello world");
 	}
 
